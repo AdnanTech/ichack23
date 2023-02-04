b7452e0b8add91e9c6da6ceacd4b7e8ee20867f9
SkollRyu <sixmilk0128@gmail.com>
2023-02-04 21:51:49 +0000

    updated class variable in app py

--- scripts/app.py
+++ scripts/app.py
@@ -4,13 +4,26 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+class Data:
+    def __init__(self):
+        self.prev_respiratory = 10
+        self.prev_puluse_ox = 80
+        self.prev_temp = 25
+    
+    def setter(self, prev_respiratory, prev_puluse_ox, prev_temp):
+        self.prev_respiratory = prev_respiratory
+        self.prev_puluse_ox = prev_puluse_ox
+        self.prev_temp = prev_temp
+    
+data = Data()
 
 
 @app.route("/api/per_min", methods=['GET'])
-def get_rand_num():
+def get_per_min():
     respiratory = random.randint(4,30)
     puluse_ox = random.randint(75,120)
     temp = random.randint(10,50)
+    data.setter(respiratory, puluse_ox, temp)
     return {
         'respiratory':  respiratory,
         'puluse_ox' : puluse_ox,
@@ -18,7 +31,7 @@ def get_rand_num():
         }
 
 @app.route("/api/per_day", methods=['GET'])
-def get_rand_num(t):
+def get_per_day():
     steps = random.randint(0,10000)
     return {
         'stpes' : steps
--- scripts/client.py
+++ scripts/client.py
@@ -3,7 +3,7 @@ import time
 
 a = []
 while True:
-    response = requests.get("http://127.0.0.1:5000/api/average")
+    response = requests.get("http://127.0.0.1:5000/api/per_min")
     data = response.text
     print(data)
     time.sleep(1)
