0a9cc0d7f0d4e14b4fde3cf973cd10e08961855d
SkollRyu <sixmilk0128@gmail.com>
2023-02-04 21:08:08 +0000

    updated

--- scripts/app.py
+++ scripts/app.py
@@ -5,23 +5,15 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/api/per_sec", methods=['GET'])
-def get_rand_num(t):
-    oxygen = random.randint(3,9)
-    respiratory = random.randint(3,9)
-    Puluse_ox = random.randint(3,9)
-    return {
-        
-        }
 
 @app.route("/api/per_min", methods=['GET'])
-def get_rand_num(t):
-    respiratory = random.randint(3,9)
-    Puluse = random.randint(3,9)
+def get_rand_num():
+    respiratory = random.randint(4,30)
+    puluse_ox = random.randint(75,120)
     temp = random.randint(10,50)
     return {
         'respiratory':  respiratory,
-        'puluse' : Puluse,
+        'puluse_ox' : puluse_ox,
         'temp' : temp
         }
 
